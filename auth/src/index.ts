62be6efd7a04664d1769138eb4662ab04c2fce39
Shakti Shah <shaktishah@Shaktis-MacBook.local>
2025-11-13 21:23:53 -0800

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { signOutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true)
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+// Error middleware
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MongoDB.');
+    } catch (err) {
+        console.log(err);
+    }
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!');
+    });
+};
+
+start();
